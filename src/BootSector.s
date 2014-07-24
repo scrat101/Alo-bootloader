3c75b9acdabb641e8c400f6ae926f7dab6478520
Alex <alex@johnrichardsonfamily.com>
2014-07-23 23:51:23 -0500

    Added hex-printing plus prints boot drive number.

--- src/BootSector.s
+++ src/BootSector.s
@@ -26,6 +26,8 @@ movw %ax, %ss
 jmpl $0x7C0, $thestart 
 
 thestart: 
+	movw $DriveNumber, %di   
+	movb %dl, (%di) 
 	movw $0x400, %sp # Lets do a 512 byte stack, eh? 
 	call ClearScreen 
 	pushw $FirstMessage
@@ -34,7 +36,17 @@ thestart:
 	# call LoadStageTwo #Load stage two 
 	pushw $LoadedMessage 
 	call PrintString 
-	add $2, %sp #Stack cleanup..................                  
+	add $2, %sp #Stack cleanup..................        
+	pushw $DriveNumberMessage 
+	call PrintString 
+	add $2, %sp 
+	movw $DriveNumber, %bx 
+	pushw (%bx) 
+	call PrintHex 
+	add $2, %sp 
+	pushw $0x2E 
+	call PrintChar  
+	add $2, %sp 
 	#jmp $0x401 #Transfer control to stage two 
 	jmp .ANotEndingLoop 
 	.ANotEndingLoop:           
@@ -46,14 +58,14 @@ LoadStageTwo:
 
 ClearScreen: 
 	pusha 
-	movw $0, %cx 
-	movw $0x0700, %ax 
-	movb $0x0F, %bh 
-	movw $0x184F, %dx 
-	DoInterrupt 0x10 
 	movb $0, %bh 
 	movw $0, %dx 
 	movb $0x02, %ah 
+	DoInterrupt 0x10 
+	movw $0, %cx 
+	movw $0x0700, %ax 
+	movb $0x07, %bh 
+	movw $0x184F, %dx 
 	DoInterrupt 0x10 
 	popa 
 	ret   
@@ -83,6 +95,31 @@ PrintString:
 		FunctionExit 
 		ret 
 		
+
+PrintHex: 
+	FunctionEntry 
+	pushw $PrintHexPreStr 
+	call PrintString 
+	add $2, %sp 
+	movw (%bp), %cx 
+	movw $4, %ax 
+	movw $HexStr, %di 
+	.TheLoopyLoop: 
+		cmp $0, %ax 
+		je .itisdone 
+		rol $4, %cx 
+		movw %cx, %bx 
+		and $0x0F, %bx 
+		add %di, %bx 
+		movw (%bx), %bx 
+		pushw %bx 
+		call PrintChar 
+		add $2, %sp 
+		dec %ax 
+		jmp .TheLoopyLoop  
+	.itisdone: 
+		FunctionExit 
+		ret 
 		
 FirstMessage: 
 	.ascii "Hello bootloader world!\r\n" 
@@ -93,8 +130,20 @@ LoadedMessage:
 	.ascii "Stage two loaded."     
 	.byte 0 
 	
+DriveNumber: 	
+	.byte 0x00 
 	
+HexStr: 
+	.ascii "0123456789ABCDEF" 
+
+PrintHexPreStr: 
+	.ascii "0x"  
+	.byte 0 
 	
+DriveNumberMessage: 
+	.ascii "\r\nDrive number: " 
+	.byte 0 
+
 .org 510 	
 	.byte 0x55 
 	.byte 0xAA 
